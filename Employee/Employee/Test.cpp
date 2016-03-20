1bf2d1576f70feaa70398b68ce5dce3d4ad7c92b
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-03-20 16:52:20 +0200

    Add more test code

--- Employee/Employee/Test.cpp
+++ Employee/Employee/Test.cpp
@@ -18,5 +18,48 @@ int main()
 
 	cout << "Total number of Employees: " << Employee::numEmployees << endl;  
 
+
+	Employee *ptrEmp[3];  // Can't create it the usual way because of the abstract pure virt function
+
+	salaryEmp->setSalary(50000);
+
+	houryEmp->setHourlyRate(1000);
+	houryEmp->setHoursWorked(30);
+
+	commissionEmp->setBaseSalary(10000);
+	commissionEmp->setRate(0.80);
+	commissionEmp->setRevenue(16000.80);
+
+	//Fill out array with object of derived class
+	ptrEmp[0] = salaryEmp;
+	ptrEmp[1] = houryEmp;
+	ptrEmp[2] = commissionEmp;
+
+	//Display salary
+	cout << endl;
+	cout << salaryEmp->name() << "'s monthly salary is " << ptrEmp[0]->salary() << endl;
+	cout << houryEmp->name() << "'s monthly salary is " << ptrEmp[1]->salary() << endl;
+	cout << commissionEmp->name() << "'s monthly salary is " << ptrEmp[2]->salary() << endl;
+	cout << endl;
+
+	//Get the total monthly salary to be paid
+
+	totalSalary = 0;
+
+	int i;
+	for (i = 0; i<3; i++)
+	{
+		ptrEmp[i]->salary()
+	}
+	cout << "Total Salary for All Employees is: " << totalSalary << endl;
+
+	//testing destroying employees
+
+	cout << endl;
+	cout << "Firing " << ptrEmp[2]->name() << endl;
+	delete ptrEmp[2];
+	cout << "Employee count: " << Employee::numEmployees << endl;
+
+
 	return 0;
 }
